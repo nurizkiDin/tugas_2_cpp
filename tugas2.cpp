d76562184015ae65dfcb4d4042a93af11915bcb8
Nurizki <nutrisi1udin@gmail.com>
2022-10-04 06:13:07 +0700

    tugas 2 cpp
    tugas niomic

---
+++ tugas2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	string niomic = "NIOMIC";
+	string belajar;
+	belajar = "C++";
+	
+	cout<<"Selamat Datang Di "<<niomic<<endl;
+	cout<<"Saya Sedang Belajar "<<belajar;
+}
